c2b9100533f66204fca60f891c232d8ca04be4b2
Stefany Sanchez <stefanysanz@icloud.com>
2020-11-05 22:55:15 -0500

    updating

--- main.js
+++ main.js
@@ -2,278 +2,234 @@ function Main() {
     console.log("main");
 
     // Get the app div
-    let app = document.getElementById("app");
+    let app = $("#app");
 
     // Create start
     let start = document.createElement("div");
-    start.id = "start";
-    app.appendChild(start);
+    $(start).attr("id", "start");
+    $(app).append(start);
 
     // Create start title
     let startTitle = document.createElement("div");
-    startTitle.id = "start-title";
-    startTitle.innerHTML = "Coding Quiz Challenge";
-    start.appendChild(startTitle);
+    $(startTitle).attr("id", "start-title");
+    $(startTitle).html("Coding Quiz Challenge");
+    $(start).append(startTitle);
 
     // Create start info
-    let startInfo = document.createElement("p");
-    startInfo.id = "start-info";
-    startInfo.innerHTML = "Test your knowledge on JavaScript with this short five question quiz. You have 5 minutes to complete it. For every question you answer correctly you will receive 5 points. When you complete the quiz sumbit your initials so your score is tracked. May the odds be ever in your favor!"; 
-    start.appendChild(startInfo);
+    let startInfo = document.createElement("div");
+    $(startInfo).attr("id", "start-info");
+    $(startInfo).html("Insert some quiz info.");
+    $(start).append(startInfo);
 
-    Error checking mail for your external accounts.
-        Details
-  
-Dismiss
-    Meet
-    New meeting
-    Join a meeting
-    Hangouts
+    // Create start button
+    let startBtn = document.createElement("button");
+    $(startBtn).attr("id", "start-btn");
+    $(startBtn).html("Start Quiz");
+    $(start).append(startBtn);
 
-    1 of 21, 055
-    Final Stuff for Quiz
-Inbox
+    // Create quiz
+    let quiz = document.createElement("div");
+    $(quiz).attr("id", "quiz");
+    $(quiz).css("height", "0px");
+    $(app).append(quiz);
 
-Andrey C.Hicks
-    Attachments
-    10: 34 PM(5 minutes ago)
-    to me
+    // Create the timer
+    let timer = document.createElement("div");
+    $(timer).attr("id", "quiz-timer");
+    $(timer).html("00:00");
+    $(quiz).append(timer);
 
+    // Create quiz question
+    let question = document.createElement("div");
+    $(question).attr("id", "quiz-question");
+    $(question).html("Insert your question here");
+    $(quiz).append(question);
 
-    2 Attachments
+    // Create answer buttons
+    let answerIDs = ["a", "b", "c", "d"];
+    let answerBtns = {};
+    for (var i = 0; i < answerIDs.length; i++) {
+        let btnID = answerIDs[i];
+        let btn = makeAnswerBtn(btnID);
+        answerBtns[btnID] = btn;
+        $(quiz).append(btn.container);
+    }
 
-    function Main() {
-        console.log("main");
+    // Create finish
+    let finish = document.createElement("div");
+    $(finish).attr("id", "finish");
+    $(finish).css("height", "0px");
+    $(app).append(finish);
 
-        // Get the app div
-        let app = $("#app");
+    // Create finish title
+    let finishTitle = document.createElement("div");
+    $(finishTitle).attr("id", "finish-title");
+    $(finishTitle).html("Quiz Complete");
+    $(finish).append(finishTitle);
 
-        // Create start
-        let start = document.createElement("div");
-        $(start).attr("id", "start");
-        $(app).append(start);
+    // Create finish score
+    let finishScore = document.createElement("div");
+    $(finishScore).attr("id", "finish-score");
+    $(finishScore).html("0");
+    $(finish).append(finishScore);
 
-        // Create start title
-        let startTitle = document.createElement("div");
-        $(startTitle).attr("id", "start-title");
-        $(startTitle).html("Coding Quiz Challenge");
-        $(start).append(startTitle);
+    // Create finish form
+    let finishForm = document.createElement("div");
+    $(finishForm).attr("id", "finish-form");
+    $(finish).append(finishForm);
 
-        // Create start info
-        let startInfo = document.createElement("div");
-        $(startInfo).attr("id", "start-info");
-        $(startInfo).html("Insert some quiz info.");
-        $(start).append(startInfo);
+    // Create finish label
+    let finishLabel = document.createElement("span");
+    $(finishLabel).attr("id", "finish-label");
+    $(finishLabel).html("Enter your initials");
+    $(finishForm).append(finishLabel);
 
-        // Create start button
-        let startBtn = document.createElement("button");
-        $(startBtn).attr("id", "start-btn");
-        $(startBtn).html("Start Quiz");
-        $(start).append(startBtn);
+    // Create finish initials
+    let finishInitials = document.createElement("input");
+    $(finishInitials).attr("id", "finish-initials");
+    $(finishInitials).attr("type", "text");
+    $(finishForm).append(finishInitials);
 
-        // Create quiz
-        let quiz = document.createElement("div");
-        $(quiz).attr("id", "quiz");
+    // Create finish button
+    let finishBtn = document.createElement("button");
+    $(finishBtn).html("Submit");
+    $(finishBtn).click(function() {
+        console.log("Submit: " + finishInitials.value);
+        $(finishInitials).attr("value", "");
+        showStart();
+    });
+    $(finishForm).append(finishBtn);
+
+    // quizTotalTime is the quiz time in seconds
+    // quizTotalTime = minutes * seconds/minute
+    let quizTotalTime = 5 * 60;
+    let quizInt;
+    let quizTime;
+
+    let questionIndex;
+    let questionInt;
+    let correct;
+    let score;
+    let scoreTotal;
+
+    function showStart() {
+        $(start).css("height", "auto");
         $(quiz).css("height", "0px");
-        $(app).append(quiz);
-
-        // Create the timer
-        let timer = document.createElement("div");
-        $(timer).attr("id", "quiz-timer");
-        $(timer).html("00:00");
-        $(quiz).append(timer);
-
-        // Create quiz question
-        let question = document.createElement("div");
-        $(question).attr("id", "quiz-question");
-        $(question).html("Insert your question here");
-        $(quiz).append(question);
-
-        // Create answer buttons
-        let answerIDs = ["a", "b", "c", "d"];
-        let answerBtns = {};
-        for (var i = 0; i < answerIDs.length; i++) {
-            let btnID = answerIDs[i];
-            let btn = makeAnswerBtn(btnID);
-            answerBtns[btnID] = btn;
-            $(quiz).append(btn.container);
-        }
-
-        // Create finish
-        let finish = document.createElement("div");
-        $(finish).attr("id", "finish");
         $(finish).css("height", "0px");
-        $(app).append(finish);
+    }
+    
+    $(startBtn).click(function startQuiz() {
+        console.log("start quiz");
 
-        // Create finish title
-        let finishTitle = document.createElement("div");
-        $(finishTitle).attr("id", "finish-title");
-        $(finishTitle).html("Quiz Complete");
-        $(finish).append(finishTitle);
+        $(start).css("height", "0px");
+        $(quiz).css("height", "auto");
 
-        // Create finish score
-        let finishScore = document.createElement("div");
-        $(finishScore).attr("id", "finish-score");
-        $(finishScore).html("0");
-        $(finish).append(finishScore);
+        questionIndex = 0;
+        correct = "";
+        score = 0;
+        scoreTotal = 0;
 
-        // Create finish form
-        let finishForm = document.createElement("div");
-        $(finishForm).attr("id", "finish-form");
-        $(finish).append(finishForm);
+        resetTimer();
 
-        // Create finish label
-        let finishLabel = document.createElement("span");
-        $(finishLabel).attr("id", "finish-label");
-        $(finishLabel).html("Enter your initials");
-        $(finishForm).append(finishLabel);
+        loadQuestion();
+    });
 
-        // Create finish initials
-        let finishInitials = document.createElement("input");
-        $(finishInitials).attr("id", "finish-initials");
-        $(finishInitials).attr("type", "text");
-        $(finishForm).append(finishInitials);
-
-        // Create finish button
-        let finishBtn = document.createElement("button");
-        $(finishBtn).html("Submit");
-        $(finishBtn).click(function () {
-            console.log("Submit: " + finishInitials.value);
-            $(finishInitials).attr("value", "");
-            showStart();
-        });
-        $(finishForm).append(finishBtn);
-
-        // quizTotalTime is the quiz time in seconds
-        // quizTotalTime = minutes * seconds/minute
-        let quizTotalTime = 5 * 60;
-        let quizInt;
-        let quizTime;
-
-        let questionIndex;
-        let questionInt;
-        let correct;
-        let score;
-        let scoreTotal;
-
-        function showStart() {
-            $(start).css("height", "auto");
-            $(quiz).css("height", "0px");
-            $(finish).css("height", "0px");
-        }
-
-        $(startBtn).click(function startQuiz() {
-            console.log("start quiz");
-
-            $(start).css("height", "0px");
-            $(quiz).css("height", "auto");
-
-            questionIndex = 0;
-            correct = "";
-            score = 0;
-            scoreTotal = 0;
-
-            resetTimer();
-
-            loadQuestion();
-        });
-
-        function loadQuestion() {
-            console.log("load question");
-            clearInterval(questionInt);
-            if (questionIndex < questions.length) {
-                let q = questions[questionIndex];
-                $(question).html(q.question);
-                for (let id in answerBtns) {
-                    let btn = answerBtns[id];
-                    let answer = q.answers[id];
-                    $(btn.answer).html(answer);
-                }
-                correct = q.correct;
-                score = q.points;
-                resetBtns();
-            } else {
-                showFinish();
-            }
-        }
-
-        function showFinish() {
-            console.log("show finish");
-            window.clearInterval(quizInt);
-            $(quiz).css("height", "0px");
-            $(finish).css("height", "auto");
-        }
-
-        function resetTimer() {
-            window.clearInterval(quizInt);
-            quizInt = setInterval(updateTimer, 1000);
-            quizTime = quizTotalTime;
-            $(timer).html("5:00");
-        }
-
-        function updateTimer() {
-            quizTime -= 1;
-            let minutes = Math.floor(quizTime / 60);
-            let seconds = quizTime % 60;
-            if (minutes <= 0 && seconds <= 0) {
-                showFinish();
-            }
-            $(timer).html(minutes + ":" + seconds);
-        }
-
-        function makeAnswerBtn(id) {
-            var container = document.createElement("div");
-            $(container).attr("class", "quiz-btn-container");
-
-            var radioBtn = document.createElement("input");
-            $(radioBtn).attr("type", "radio");
-            $(container).append(radioBtn);
-
-            var answer = document.createElement("span");
-            $(answer).html("Answer");
-            $(container).append(answer);
-
-            $(radioBtn).click(function () {
-                console.log(id);
-                if (id == correct) {
-                    console.log("result: correct");
-                    console.log("score: " + score);
-                    console.log("scoreTotal: " + scoreTotal);
-                    scoreTotal += score;
-                    $(finishScore).html("Score: " + scoreTotal);
-                    $(answer).css("color", "green");
-                } else {
-                    $(answer).css("color", "red");
-                    quizTime -= 5;
-                    if (quizTime <= 0) {
-                        quizTime = 0;
-                    }
-                }
-                questionIndex++;
-                disableBtns();
-                questionInt = setInterval(loadQuestion, 1000);
-            });
-
-            return {
-                container: container,
-                answer: answer,
-                radioBtn: radioBtn,
-            };
-        }
-
-        function disableBtns() {
+    function loadQuestion() {
+        console.log("load question");
+        clearInterval(questionInt);
+        if (questionIndex < questions.length) {
+            let q = questions[questionIndex];
+            $(question).html(q.question);
             for (let id in answerBtns) {
                 let btn = answerBtns[id];
-                btn.radioBtn.disabled = true;
+                let answer = q.answers[id];
+                $(btn.answer).html(answer);
             }
-        }
-
-        function resetBtns() {
-            for (let id in answerBtns) {
-                let btn = answerBtns[id];
-                btn.radioBtn.disabled = false;
-                btn.radioBtn.checked = false;
-                $(btn.answer).css("color", "black");
-            }
+            correct = q.correct;
+            score = q.points;
+            resetBtns();
+        } else {
+            showFinish();
         }
     }
+
+    function showFinish() {
+        console.log("show finish");
+        window.clearInterval(quizInt);
+        $(quiz).css("height", "0px");
+        $(finish).css("height", "auto");
+    }
+
+    function resetTimer() {
+        window.clearInterval(quizInt);
+        quizInt = setInterval(updateTimer, 1000);
+        quizTime = quizTotalTime;
+        $(timer).html("5:00");
+    }
+
+    function updateTimer() {
+        quizTime -= 1;
+        let minutes = Math.floor(quizTime/60);
+        let seconds = quizTime%60;
+        if (minutes <=0 && seconds <= 0) {
+            showFinish();
+        } 
+        $(timer).html(minutes + ":" + seconds);
+    }
+
+    function makeAnswerBtn(id) {
+        var container = document.createElement("div");
+        $(container).attr("class", "quiz-btn-container");
+
+        var radioBtn = document.createElement("input");
+        $(radioBtn).attr("type", "radio");
+        $(container).append(radioBtn);
+
+        var answer = document.createElement("span");
+        $(answer).html("Answer");
+        $(container).append(answer);
+
+        $(radioBtn).click(function() {
+            console.log(id);
+            if (id == correct) {
+                console.log("result: correct");
+                console.log("score: " + score);
+                console.log("scoreTotal: " + scoreTotal);
+                scoreTotal += score;
+                $(finishScore).html("Score: " + scoreTotal);
+                $(answer).css("color", "green");
+            } else {
+                $(answer).css("color", "red");
+                quizTime -= 5;
+                if (quizTime <= 0) {
+                    quizTime = 0;
+                }
+            }
+            questionIndex++;
+            disableBtns();
+            questionInt = setInterval(loadQuestion, 1000);
+        });
+        
+        return {
+            container: container,
+            answer: answer,
+            radioBtn: radioBtn,
+        };
+    }
+
+    function disableBtns() {
+        for (let id in answerBtns) {
+            let btn = answerBtns[id];
+            btn.radioBtn.disabled = true;
+        }
+    }
+
+    function resetBtns() {
+        for (let id in answerBtns) {
+            let btn = answerBtns[id];
+            btn.radioBtn.disabled = false;
+            btn.radioBtn.checked = false;
+            $(btn.answer).css("color", "black");
+        }
+    }
+}
--- questions.js
+++ questions.js
@@ -1,35 +1,35 @@
 let questions = [
-  {
-    question: "This CSS selector styles a unique element.",
-    answers: {
-      a: "id",
-      b: "class",
-      c: "value",
-      d: "type"
+    {
+        question: "This CSS selector styles a unique element.",
+        answers: {
+            a: "id",
+            b: "class",
+            c: "value",
+            d: "type"
+        },
+        correct: "a",
+        points: 5
     },
-    correct: "a",
-    points: 5
-  },
-  {
-    question: "This CSS selector can style multiple elements.",
-    answers: {
-      a: "id",
-      b: "class",
-      c: "value",
-      d: "type"
+    {
+        question: "This CSS selector can style multiple elements.",
+        answers: {
+            a: "id",
+            b: "class",
+            c: "value",
+            d: "type"
+        },
+        correct: "b",
+        points: 5
     },
-    correct: "b",
-    points: 5
-  },
-  {
-    question: "This javascript method creates an element.",
-    answers: {
-      a: "create()",
-      b: "makeElement()",
-      c: "createElement()",
-      d: "make()"
-    },
-    correct: "c",
-    points: 5
-  }
+    {
+        question: "This javascript method creates an element.",
+        answers: {
+            a: "create()",
+            b: "makeElement()",
+            c: "createElement()",
+            d: "make()"
+        },
+        correct: "c",
+        points: 5
+    }
 ];
