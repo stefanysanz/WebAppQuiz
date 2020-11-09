135c2dc5c23ecc10b0cbd325b8ae80df3ff50682
Stefany Sanchez <stefanysanz@icloud.com>
2020-11-09 10:57:52 -0500

    updates

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function Main() {
     // Create start info
     let startInfo = document.createElement("p");
     $(startInfo).attr("id", "start-info");
-    $(startInfo).html(" Test your knowledge & take this short 5 question quiz. You will have five minutes to complete it & will receive 5 points for every question you answer correctly. Dont forget to sumbit your initials so we can track the highest score. Good Luck! :) ");
+    $(startInfo).html("Test your knowledge & take this short 5 question quiz.");
     $(start).append(startInfo);
 
     // Create start button
